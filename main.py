3cbfed365f614f2a9305c895f5756672a9d24dd3
Noah Honold <noah.honold@gmx.de>
2024-02-01 14:45:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import turtle
 
-# Create a turtle
+print("Hello World")
+
+import turtle
+
 t = turtle.Turtle()
+
+t.color("red")
+for i in range(2):
+    t.forward(90)  
+    t.right(90)    
+    t.forward(50)   
+    t.right(90)     
+
+
+turtle.done()
