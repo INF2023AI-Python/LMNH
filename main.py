5cdf6b5fd61d7263d1401f94f0a7ce6deac1baac
LucaMllr <158320869+LucaMllr@users.noreply.github.com>
2024-02-01 15:16:20 +0100

    Moin

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import turtle
+import random
 
 # Create a turtle
 t = turtle.Turtle()
+t.speed(0)
+colors = ["blue","green","yellow","red","purple"]
+
+for i in range(500):
+ t.color(random.choice(colors))
+ t.forward(1)
+ t.right(30)
+ t.backward(1)
+ t.circle(i)
+
+turtle.done()
